eeaed81cb8ba24cda5a44867c3f3eb3bdd16417a
Jae Yi <jaehyi@amazon.com>
2024-12-08 20:54:32 -0500

    Includes minor optimizataion for object_tagger.py

--- diode-workstream/cross-account/validation-account/lambda/object_tagger.py
+++ diode-workstream/cross-account/validation-account/lambda/object_tagger.py
@@ -1,56 +1,65 @@
 import json
-import boto3
+import logging
 import os
+
+import boto3  # type: ignore
+from botocore.config import Config  # type: ignore
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+config = Config(retries={"max_attempts": 5, "mode": "standard"})
+S3_CLIENT = boto3.client('s3', config=config)
+SQS_CLIENT = boto3.client('sqs', config=config)
+
+
 def add_tags(bucket, key):
     gov_poc = os.environ['GOV_POC']
     data_owner = os.environ['DATA_OWNER']
     key_owner = os.environ['KEY_OWNER']
     cds_profile = os.environ['CDS_PROFILE']
+
+    # Amazon S3 limits the maximum number of tags to 10 tags per object
     tags = [
         {'Key': 'GovPOC', 'Value': gov_poc},
         {'Key': 'DataOwner', 'Value': data_owner},
         {'Key': 'KeyOwner', 'Value': key_owner},
         {'Key': 'CDSProfile', 'Value': cds_profile}
     ]
-    
-    s3_client = boto3.client('s3')
-    response = s3_client.put_object_tagging(
-        Bucket = bucket,
-        Key = key,
-        Tagging = {
+
+    response = S3_CLIENT.put_object_tagging(
+        Bucket=bucket,
+        Key=key,
+        Tagging={
             'TagSet': tags
-        }
-        )
-    print(f'Tagging Response = {response}')
+        },
+        # TODO: We should add this for enhanced security
+        # ExpectedBucketOwner
+    )
+    logger.info(f'Tagging Response = {response}')
+
+
 def send_to_sqs(bucket, key):
-    sqs_client = boto3.client('sqs')
     queue_url = os.environ['QUEUE_URL']
-    
-    response = sqs_client.send_message(
-        QueueUrl = queue_url,
-        MessageBody = json.dumps({
+
+    response = SQS_CLIENT.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps({
             'detail': {
-                'requestParameters':{
-                    'bucketName':bucket,
+                'requestParameters': {
+                    'bucketName': bucket,
                     'key': key
                 }
             }
         })
-        
-        )
-    print(f'sqs response: {response}')
+
+    )
+    logger.info(f'sqs response: {response}')
+
 
 def lambda_handler(event, context):
-    
+
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     key = event["Records"][0]["s3"]["object"]["key"]
-    
-    add_tags(bucket,key)
+
+    add_tags(bucket, key)
     send_to_sqs(bucket, key)
-    
-    
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
