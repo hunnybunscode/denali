54ce9165293d7ddbd5b3b6a41313e302756fac28
Jae Yi <jaehyi@amazon.com>
2024-12-15 00:43:40 -0500

    Optimizes presigner functions

--- diode-workstream/cross-account/validation-account/lambda/api_gw_lambda_functions/presigner.py
+++ diode-workstream/cross-account/validation-account/lambda/api_gw_lambda_functions/presigner.py
@@ -1,31 +1,36 @@
 import json
-import boto3
 import logging
+
+import boto3  # type: ignore
+from botocore.config import Config  # type: ignore
+
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+config = Config(retries={"max_attempts": 5, "mode": "standard"})
+S3_CLIENT = boto3.client("s3", config=config)
+
 
 def lambda_handler(event, context):
-    logger.info(f'Event: {event}')
-    print(f'Event: {event}')
-    
-    bucket = event["queryStringParameters"]['bucket']
-    key = event["queryStringParameters"]['key']
-    logger.info(f'Bucket: {bucket}')
-    logger.info(f'Key: {key}')
+    logger.info(f"Event: {json.dumps(event, default=str)}")
 
-    
-    s3_client = boto3.client('s3')
-    
-    response_body = s3_client.generate_presigned_post(
-        Bucket = bucket,
-        Key = key
-        )
-    #response_body = json.dumps(response_body)
-    logger.info(f'Response Body: {response_body}')
+    bucket = event["queryStringParameters"]["bucket"]
+    key = event["queryStringParameters"]["key"]
+
+    logger.info(f"Bucket: {bucket}")
+    logger.info(f"Key: {key}")
+
+    response_body = S3_CLIENT.generate_presigned_post(
+        Bucket=bucket,
+        Key=key
+    )
+
+    logger.info(f"Response Body: {response_body}")
 
     response = {
-        "statusCode":200,
-        "body":json.dumps(response_body),
+        "statusCode": 200,
+        "body": json.dumps(response_body),
         "isBase64Encoded": False
     }
-    
+
     return response
