28ab044150d05d2386e8dd759315668caf6c2412
Jae Yi <jaehyi@amazon.com>
2024-12-17 23:12:24 -0500

    Minor updates to object_tagger.py

--- diode-workstream/cross-account/validation-account/lambda/object_tagger.py
+++ diode-workstream/cross-account/validation-account/lambda/object_tagger.py
@@ -7,59 +7,63 @@ from botocore.config import Config  # type: ignore
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+
+GOV_POC = os.environ["GOV_POC"]
+DATA_OWNER = os.environ["DATA_OWNER"]
+KEY_OWNER = os.environ["KEY_OWNER"]
+CDS_PROFILE = os.environ["CDS_PROFILE"]
+
+QUEUE_URL = os.environ["QUEUE_URL"]
+
 config = Config(retries={"max_attempts": 5, "mode": "standard"})
-S3_CLIENT = boto3.client('s3', config=config)
-SQS_CLIENT = boto3.client('sqs', config=config)
+S3_CLIENT = boto3.client("s3", config=config)
+SQS_CLIENT = boto3.client("sqs", config=config)
 
 
-def add_tags(bucket, key):
-    gov_poc = os.environ['GOV_POC']
-    data_owner = os.environ['DATA_OWNER']
-    key_owner = os.environ['KEY_OWNER']
-    cds_profile = os.environ['CDS_PROFILE']
+def lambda_handler(event, context):
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
 
+    add_tags(bucket, key)
+    send_to_sqs(bucket, key)
+
+
+def add_tags(bucket: str, key: str):
     # Amazon S3 limits the maximum number of tags to 10 tags per object
     tags = [
-        {'Key': 'GovPOC', 'Value': gov_poc},
-        {'Key': 'DataOwner', 'Value': data_owner},
-        {'Key': 'KeyOwner', 'Value': key_owner},
-        {'Key': 'CDSProfile', 'Value': cds_profile}
+        {"Key": "GovPOC", "Value": GOV_POC},
+        {"Key": "DataOwner", "Value": DATA_OWNER},
+        {"Key": "KeyOwner", "Value": KEY_OWNER},
+        {"Key": "CDSProfile", "Value": CDS_PROFILE}
     ]
 
     response = S3_CLIENT.put_object_tagging(
         Bucket=bucket,
         Key=key,
         Tagging={
-            'TagSet': tags
+            "TagSet": tags
         },
+
         # TODO: We should add this for enhanced security
         # ExpectedBucketOwner
     )
-    logger.info(f'Tagging Response = {response}')
+
+    logger.info(f"PutObjectTagging response = {response}")
 
 
 def send_to_sqs(bucket, key):
-    queue_url = os.environ['QUEUE_URL']
-
     response = SQS_CLIENT.send_message(
-        QueueUrl=queue_url,
+        QueueUrl=QUEUE_URL,
         MessageBody=json.dumps({
-            'detail': {
-                'requestParameters': {
-                    'bucketName': bucket,
-                    'key': key
+            "detail": {
+                "requestParameters": {
+                    "bucketName": bucket,
+                    "key": key
                 }
             }
         })
 
     )
-    logger.info(f'sqs response: {response}')
 
-
-def lambda_handler(event, context):
-
-    bucket = event["Records"][0]["s3"]["bucket"]["name"]
-    key = event["Records"][0]["s3"]["object"]["key"]
-
-    add_tags(bucket, key)
-    send_to_sqs(bucket, key)
+    logger.info(f"SendMessage response: {response}")
