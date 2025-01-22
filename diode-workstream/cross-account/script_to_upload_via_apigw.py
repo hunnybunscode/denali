1797bd405dbea8554855584e087754b9469a1c38
Jae Yi <jaehyi@amazon.com>
2025-01-22 13:51:30 -0500

    Adds a script that can be used to upload objects using an API Gateway endpoint

---
+++ diode-workstream/cross-account/script_to_upload_via_apigw.py
@@ -0,0 +1,136 @@
+# References:
+# https://github.com/aws-samples/sigv4-signing-examples/blob/main/sdk/python/main.py
+# https://github.com/boto/botocore/blob/develop/botocore/auth.py
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
+import argparse
+import os
+from pathlib import Path
+
+import boto3  # type: ignore
+import requests  # type: ignore
+from botocore.auth import SigV4Auth  # type: ignore
+from botocore.awsrequest import AWSRequest  # type: ignore
+
+
+###################################################################################
+# -----------------     Set these environment variables     -----------------
+# For example: export AWS_ACCESS_KEY_ID="abcdefg"
+
+ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+# Set this if using temporary credentials
+SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+# Defaults to us-gov-west-1, if not set
+REGION = os.getenv("AWS_DEFAULT_REGION", "us-gov-west-1")
+# For example: "https://abcdef.execute-api.us-gov-west-1.amazonaws.com/prod/upload"
+INVOKE_URL = os.getenv("INVOKE_URL")
+
+###################################################################################
+
+
+def main(bucket_name: str, key_name: str, prefix: str, filepath: str, credentials):
+    method = "POST"
+    service = "execute-api"
+
+    params = dict(bucket=bucket_name, key=prefix + key_name)
+    request = AWSRequest(method, INVOKE_URL, params=params)
+    SigV4Auth(credentials, service, REGION).add_auth(request)
+    presigned_url = get_presigned_post_url(method, request, params)
+    upload_file(presigned_url, filepath, params)
+
+
+def get_credentials():
+    session_config = dict(
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=REGION,
+    )
+    if SESSION_TOKEN:
+        session_config["aws_session_token"] = SESSION_TOKEN
+
+    session = boto3.Session(**session_config)
+    return session.get_credentials()
+
+
+def get_presigned_post_url(method: str, request, params: dict):
+    try:
+        response = requests.request(
+            method,
+            INVOKE_URL,
+            headers=dict(request.headers),
+            params=params,
+            timeout=10,
+        )
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"Could not get presigned post url: {e}")
+        raise
+
+
+def upload_file(presigned_url: dict, file_path: str, params: dict):
+    with open(file_path, "rb") as f:
+        files = {"file": (file_path, f)}
+        response = requests.post(
+            presigned_url["url"],
+            data=presigned_url["fields"],
+            files=files,
+            timeout=300,  # Adjust this as necessary
+        )
+
+    try:
+        # Successful HTTP status code is 204
+        response.raise_for_status()
+        print(f"SUCCESS: {file_path} uploaded to {params['bucket']}/{params['key']}")
+    except Exception as e:
+        print(f"Could not upload file: {e}")
+
+
+def get_resolved_file_path(path: Path):
+    """
+    Returns the resolved file path
+    """
+    return str(path.resolve())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Gets a pre-signed URL from the specified API Gateway endpoint and uploads a file to the specified bucket",  # noqa: E501
+        allow_abbrev=False,
+    )
+
+    parser.add_argument(
+        "--bucket",
+        type=str,
+        help="Specify the name of the bucket to generate a pre-signed URL for",
+    )
+
+    parser.add_argument(
+        "--filepath",
+        type=str,
+        help="Specify the path of the file to upload",
+    )
+
+    parser.add_argument(
+        "--prefix",
+        type=str,
+        default="",
+        help="Specify the prefix to append to the file. Defaults to an empty string",
+    )
+
+    args = parser.parse_args()
+
+    path = Path(args.filepath)
+    prefix: str = args.prefix.strip()
+    if prefix and not prefix.endswith("/"):
+        prefix = prefix + "/"
+    filepath = get_resolved_file_path(path)
+    credentials = get_credentials()
+
+    main(
+        bucket_name=args.bucket,
+        key_name=path.name,
+        prefix=prefix,
+        filepath=filepath,
+        credentials=credentials,
+    )
