d38a6dd3ecbc30e219c9ab2a941094e7a7b9e41e
pmatiuk <pmatiuk@amazon.com>
2025-07-08 12:43:28 -0400

    added cdk-nag and other extra fixes

--- vpc-endpoints/bin/vpc-endpoints.ts
+++ vpc-endpoints/bin/vpc-endpoints.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
+import { AwsSolutionsChecks, NagSuppressions, NIST80053R5Checks } from 'cdk-nag';
 import { Aspects } from 'aws-cdk-lib';
 import { VpcEndpointsStack } from '../lib/vpc-endpoints-stack';
 import { loadConfig } from '../lib/config';
@@ -16,17 +16,26 @@ const tags = {
   "Environment": env,
 };
 
+// Apply tags to all resources in the app
+Object.entries(tags).forEach(([key, value]) => {
+  cdk.Tags.of(app).add(key, value);
+});
+
 const stack = new VpcEndpointsStack(app, 'VpcEndpointsStack', {
   config,
   env: {
     account: config.environment.account,
     region: config.environment.region,
   },
-  tags,
   ...(process.env.CUSTOM_TOOLKIT && {
-    toolkitStackName: process.env.CUSTOM_TOOLKIT
+    synthesizer: new cdk.DefaultStackSynthesizer({
+      bootstrapStackVersionSsmParameter: `/cdk-bootstrap/${process.env.CUSTOM_TOOLKIT}/version`,
+      fileAssetsBucketName: `${process.env.CUSTOM_TOOLKIT}-assets-\${AWS::AccountId}-\${AWS::Region}`,
+      bucketPrefix: '',
+    })
   }),
 });
 
 Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
-console.log('CDK-nag applied to app - should show findings during deployment');
+Aspects.of(app).add(new NIST80053R5Checks({ verbose: true }));
+console.log('CDK-nag applied to app - AWS Solutions and NIST 800-53 R5 checks enabled');
